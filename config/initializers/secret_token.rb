436fab7c11286d3da6e6733299f16707a63e3707
Nick Bucciarelli <nick@jackrussellsoftware.com>
2010-10-05 09:59:07 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicSite::Application.config.secret_token = 'c7906d214d679bdba46b5916a5f74ec4924356551c07ab9d672e17bbeddd30030f8e5afe5cffb9d320c1364073b86cf737a5d6e81baffeb4ccf84df1b69029c8'
