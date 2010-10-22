0aae6afdcedf5d37d8a7c5eac9f220433b6a8141
mdms-scott <mdms.scott@gmail.com>
2010-10-22 14:43:33 -0400

    songs displayed on dashbord and pagination

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem 'mysql2', '0.2.3'
 gem 'workflow', '~> 0.7.0'
 
 gem 'devise', '1.1.2'
+gem 'will_paginate', '>=3.0.pre'
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
   
   def index
+    @songs = Song.paginate :page => params[:page], :per_page => 10
+    
     respond_to do |wants|
       wants.html
     end
