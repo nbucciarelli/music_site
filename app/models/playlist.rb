4ab26c6d7c951e1c02f38aa693b7a95d51a1d4c8
Nick Bucciarelli <nick@jackrussellsoftware.com>
2010-11-04 19:14:48 -0400

    Fixed minor error during playlist creats

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,13 +2,7 @@ class Playlist < ActiveRecord::Base
   
   belongs_to :user
   
-  before_create :set_song_defaults
-  
   serialize :song_ids
-    
-  def set_song_defaults
-    self.song_ids = ['']
-  end
   
   def self.songs_in(playlist)
     songs = []
